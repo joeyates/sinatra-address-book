742e862e135dcdebe73e44239d0557c87bf65cb0
Joe Yates <joe.g.yates@gmail.com>
2013-03-30 11:45:39 +0100

    Example of passing a parameter to a template

--- address-book.rb
+++ address-book.rb
@@ -10,5 +10,11 @@ class AddressBook < Sinatra::Base
   get '/hello' do
     'Hello!'
   end
+
+  # Pass a parameter to a template
+  get '/template_parameter' do
+    some_value = 'Got this from the handler'
+    slim :template_parameter, locals: {some_value: some_value}
+  end
 end
 
