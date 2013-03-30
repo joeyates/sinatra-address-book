0b908e25fbac3402a562d496c3aec837b02c0d67
Joe Yates <joe.g.yates@gmail.com>
2013-03-30 12:35:59 +0100

    An example of a POST with CSRF pprotection

--- address-book.rb
+++ address-book.rb
@@ -6,6 +6,11 @@ class AddressBook < Sinatra::Base
     enable :inline_templates
   end
 
+  # Always create a CSRF token
+  before do
+    session[:csrf] ||= Rack::Protection::Base.new(self).random_string
+  end
+
   # The home page
   get '/' do
     slim :home
@@ -24,6 +29,19 @@ class AddressBook < Sinatra::Base
 
   get '/inline_template' do
     slim :my_inline_template
+  end
+
+  ########################################
+  # POST: Protecting from CSRF attacks
+
+  # A form with CSRF protection
+  get '/username' do
+    slim :username, locals: {username: 'myuser'}
+  end
+
+  # Receive a POST - this gets through if the CSRF token is correct
+  post '/username' do
+    "Your new username is '#{params[:username]}'"
   end
 end
 
--- config.ru
+++ config.ru
@@ -1,5 +1,15 @@
 $: << File.dirname(__FILE__)
+require 'rack/protection'
 require 'address-book'
+
+# For a real application, keep this value in an environment variable.
+# On Heroku, use a config var:
+# https://devcenter.heroku.com/articles/config-vars
+my_secret = 'caae90dc173f748114d3913ed40fc58ab76411d9f4aecec86b63ab2c05352e61'
+
+# Set up sessions and protection (including from CSRF attacks)
+use Rack::Session::Cookie, secret: my_secret
+use Rack::Protection, use: :authenticity_token
 
 run AddressBook.new
 
